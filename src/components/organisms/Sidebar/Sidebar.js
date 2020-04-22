2c8eac379a81a3b15c83eb31954dc8a95db8225f
marek3289 <marek2494@o2.pl>
2020-04-22 13:36:54 +0200

    added content folder

--- src/components/organisms/Sidebar/Sidebar.js
+++ src/components/organisms/Sidebar/Sidebar.js
@@ -5,69 +5,7 @@ import Modal from 'components/organisms/Modal/Modal';
 import Tag from 'components/molecules/Tag/Tag';
 import Button from 'components/atoms/Button/Button';
 import Paragraph from 'components/atoms/Paragraph/Paragraph';
-
-const tags = [
-  {
-    name: 'All',
-    isActive: true,
-  },
-  {
-    name: 'Animation',
-    isActive: false,
-  },
-  {
-    name: 'Art Direction',
-    isActive: false,
-  },
-  {
-    name: 'Branding',
-    isActive: false,
-  },
-  {
-    name: 'Graphic Design',
-    isActive: false,
-  },
-  {
-    name: 'Iconography',
-    isActive: false,
-  },
-  {
-    name: 'Illustration',
-    isActive: false,
-  },
-  {
-    name: 'Mobility',
-    isActive: false,
-  },
-  {
-    name: 'Marvel',
-    isActive: false,
-  },
-  {
-    name: 'Prototyping',
-    isActive: false,
-  },
-  {
-    name: 'Project Management',
-    isActive: false,
-  },
-  {
-    name: 'User Research',
-    isActive: false,
-  },
-  {
-    name: 'UI Design',
-    isActive: false,
-  },
-  {
-    name: 'UX Design',
-    isActive: false,
-  },
-  {
-    name: 'Video Editing',
-    isActive: false,
-  },
-];
+import { tags } from 'assets/content';
 
 const StyledWrapper = styled.aside`
   width: 250px;
