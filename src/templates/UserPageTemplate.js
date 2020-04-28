9b296ab947e685a5a64d1bb042969f167af42a46
marek3289 <marek2494@o2.pl>
2020-04-28 10:54:45 +0200

    updated component with react-helmet

--- src/templates/UserPageTemplate.js
+++ src/templates/UserPageTemplate.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
 import NavHeader from 'components/organisms/NavHeader/NavHeader';
 
 const UserPageTemplate = ({ children }) => (
   <>
+    <Helmet>
+      <title>Network</title>
+    </Helmet>
     <NavHeader />
     {children}
   </>
